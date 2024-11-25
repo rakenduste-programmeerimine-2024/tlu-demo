14998bbd9ae0fd6a1e5db71fd54aaca4be6d97ee
Jarl Koha <146342562+jarkoha@users.noreply.github.com>
2024-11-25 11:08:09 +0200

    feat(lang): sign-in, forgot password page translated

--- app/(auth-pages)/forgot-password/page.tsx
+++ app/(auth-pages)/forgot-password/page.tsx
@@ -14,24 +14,23 @@ export default async function ForgotPassword(props: {
     <>
       <form className="flex-1 flex flex-col w-full gap-2 text-foreground [&>input]:mb-6 min-w-64 max-w-64 mx-auto">
         <div>
-          <h1 className="text-2xl font-medium">Reset Password</h1>
+          <h1 className="text-2xl font-medium">Taasta parool</h1>
           <p className="text-sm text-secondary-foreground">
-            Already have an account?{" "}
+            Kas sul on juba kasutaja?{" "}
             <Link className="text-primary underline" href="/sign-in">
-              Sign in
+              Logi sisse
             </Link>
           </p>
         </div>
         <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-          <Label htmlFor="email">Email</Label>
-          <Input name="email" placeholder="you@example.com" required />
+          <Label htmlFor="email">E-mail</Label>
+          <Input name="email" placeholder="mari@maasikas.com" required />
           <SubmitButton formAction={forgotPasswordAction}>
-            Reset Password
+            Taasta parool
           </SubmitButton>
           <FormMessage message={searchParams} />
         </div>
       </form>
-      <SmtpMessage />
     </>
   );
 }
--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -9,33 +9,33 @@ export default async function Login(props: { searchParams: Promise<Message> }) {
   const searchParams = await props.searchParams;
   return (
     <form className="flex-1 flex flex-col min-w-64">
-      <h1 className="text-2xl font-medium">Sign in</h1>
+      <h1 className="text-2xl font-medium">Logi sisse</h1>
       <p className="text-sm text-foreground">
-        Don't have an account?{" "}
+        Ei ole veel kasutajat?{" "}
         <Link className="text-foreground font-medium underline" href="/sign-up">
-          Sign up
+          Logi sisse
         </Link>
       </p>
       <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-        <Label htmlFor="email">Email</Label>
-        <Input name="email" placeholder="you@example.com" required />
+        <Label htmlFor="email">E-mail</Label>
+        <Input name="email" placeholder="mari@maasikas.com" required />
         <div className="flex justify-between items-center">
-          <Label htmlFor="password">Password</Label>
+          <Label htmlFor="password">Parool</Label>
           <Link
             className="text-xs text-foreground underline"
             href="/forgot-password"
           >
-            Forgot Password?
+            Unustasid parooli?
           </Link>
         </div>
         <Input
           type="password"
           name="password"
-          placeholder="Your password"
+          placeholder="Sinu parool"
           required
         />
         <SubmitButton pendingText="Signing In..." formAction={signInAction}>
-          Sign in
+          Logi sisse
         </SubmitButton>
         <FormMessage message={searchParams} />
       </div>
