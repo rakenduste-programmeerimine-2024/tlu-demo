ab9a42a4e9c83c5a7341846b8b9fc92077b2df84
Egle Vestrik <egleves@tlu.ee>
2024-12-02 15:29:57 +0200

    feat:add admissions page with akordion

---
+++ app/dtiAboutUs/page.tsx
@@ -0,0 +1,35 @@
+"use client"; // Ensure this component runs on the client side
+import React, { useState, useEffect } from "react"
+const App: React.FC = () => {
+  return (
+    <div>
+      <h5 className="justify-center flex text-6xl" >Instituut</h5>
+      <br></br>
+      <p>Digitehnoloogiate instituudi ajalugu algas 1. septembril 2015, mil ühinesid informaatika instituut, infoteaduste instituut ning matemaatika-loodusteaduste instituudi matemaatika osakond. Direktorina asus tööle Peeter Normak. Õppe- ja teadustegevus korraldati viie akadeemilise suuna alla: haridustehnoloogia (suuna juht haridustehnoloogia keskuse juhataja Linda Helene Sillat), infoteadused (suuna juht prof Sirje Virkus), inimese ja arvuti interaktsioon (suuna juht prof David Lamas), matemaatika ja matemaatika didaktika (suuna juht dots Mart Abel) ja rakendusinformaatika (suuna juht prof Peeter Normak). Instituudi nõukogu esimeheks valiti Andrus Rinde.</p>
+      <br></br>
+
+
+      
+      <br></br>
+      <p>Soovides saavutada paremat kooskõla ühiskonna vajadustega, oleme võtnud eesmärgiks saada targa eluviisi eestvedajaks Eestis. Tark eluviis tähendab eelkõige teaduspõhiseid otsuseid nii ühiskonna kui ka kodaniku paremaks toimetulekuks.</p>
+      <br></br>
+      <p>Tallinna Ülikool on viimase 14 aastaga liitnud endaga hulga õppe- ja teadusasutusi. See on oluliselt laiendanud ülikooli vastutusvaldkondade skaalat, mis täna ulatub õpetajakoolitusest koreograafiani.</p>
+      <br></br>
+      <p>Ülikooli viis fookusvaldkonda on: haridusuuendus, digi- ja meediakultuur, kultuurilised kompetentsid, terve ja jätkusuutlik eluviis ning ühiskond ja avatud valitsemine.</p>
+      <br></br>
+      <p>Alates 1. septembrist 2015 esindab igat valdkonda vastav instituut, kuuenda instituudi ülesandeks on kõigi nende instituutide toetamine digitaaltehnoloogiate ja analüütikaga. Lisaks on ülikoolil regionaalne kolledž Haapsalus.</p>
+      <br></br>
+      <hr></hr>
+      <br></br>
+      <p>Tallinna Ülikool, Eesti ülikoolina, pakub ja arendab eelkõige eestikeelset ja Eesti vajadustest lähtuvat haridust ja teadust. Arvestades kõrghariduses toimuvat, on oluline roll ka rahvusvahelistumisel. </p>
+      <br></br>
+      <p>Tallinna Ülikooli teadlased juhivad ja osalevad Euroopa Komisjoni, EU regionaalprogrammi ja Erasmus+ poolt rahastatud projektis. Ülikoolil on lepingud 43 partnerülikooliga 23 riigist ja üle 400 Erasmus+ partnerlepingu, oleme kaheksa rahvusvahelise kõrgharidusasutusi ühendava võrgustiku liige ja meie teadlased ja õppejõud panustavad aktiivselt oma erialaste võrgustike tegemistesse.</p>
+      <br></br>
+      <p>Tallinna Ülikool, targa eluviisi eestvedajana, väärtustab ka oma liikmete vaba aega: ülikoolil on töötajatele, õppejõududele ning vilistlastele mõeldud kirjandusauhind, Eesti aasta orkestriks valitud sümfooniaorkester, rahvusvaheliselt tunnustatud koorid ja kultuurikollektiivid ning mitu Eesti meistriliiga tasemel spordivõistkonda.</p>
+
+    </div>
+
+  );
+};
+
+export default App;
