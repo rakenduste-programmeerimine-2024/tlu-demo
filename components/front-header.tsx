611da28932e8e9c4ef6ec213bfa8d6b863c7aff6
Rain Erik Elias <90192521+RainErikE@users.noreply.github.com>
2024-12-02 15:07:19 +0200

    fix: front header rollback

--- components/front-header.tsx
+++ components/front-header.tsx
@@ -1,47 +1,22 @@
-"use client";
-
 import Image from "next/image";
-import { useState } from "react";
-import LoginForm from "@/components/login/login-form";
+import Socialmedia from "./socialmedia-buttons";
+
+export function MainLogo() {
+	return (
+		<div>
+			<Image src="/logos/TLU-uldlogo.png" alt="Logo" width="300" height="60" className="block dark:hidden" />
+			<Image src="/logos/TLU-uldlogo-valge.png" alt="Logo" width="300" height="60" className="hidden dark:block" />
+			<Socialmedia />
+		</div>
+	);
+}
 
 export default function FrontHeader() {
-  const [isLoginOpen, setIsLoginOpen] = useState(false);
-
-  return (
-    <div className="w-full flex flex-col items-center justify-center relative">
-      <div className="w-full flex justify-center relative">
-        {/* Logo Section */}
-        <Image
-          src="/logos/TLU-uldlogo.png"
-          alt="Logo"
-          width="300"
-          height="60"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/logos/TLU-uldlogo-valge.png"
-          alt="Logo"
-          width="300"
-          height="60"
-          className="hidden dark:block"
-        />
-      </div>
-      <div className="absolute top-5 right-5 cursor-pointer">
-        {/* User Icon */}
-        <Image
-          src="/icons/user.png"
-          alt="User"
-          width={20}
-          height={20}
-          onClick={() => setIsLoginOpen(!isLoginOpen)}
-        />
-        {/* Login Form */}
-        {isLoginOpen && (
-          <div className="absolute right-0 mt-2 bg-white shadow-md rounded-md w-64 p-4 z-10">
-            <LoginForm message={{ message: "" }} />
-          </div>
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<>
+			<div className="w-full flex flex-col items-center justify-center">
+				<MainLogo />
+			</div>
+		</>
+	);
 }
